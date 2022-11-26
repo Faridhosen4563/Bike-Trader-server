dba8c6c6c38ad921631c785cd01c73734a7a633a
Faridhosen4563 <faridhosen790@gmail.com>
2022-11-26 17:02:23 +0600

    payment system add and stroge payment in database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ async function run() {
     const bikesCollection = client.db("BikeTrader").collection("bikes");
     const usersCollection = client.db("BikeTrader").collection("users");
     const bookingsCollection = client.db("BikeTrader").collection("bookings");
+    const paymentCollection = client.db("BikeTrader").collection("payment");
 
     app.get("/categories", async (req, res) => {
       const query = {};
@@ -38,10 +40,24 @@ async function run() {
       res.send(bikes);
     });
 
+    app.get("/products", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const products = await bikesCollection.find(query).toArray();
+      res.send(products);
+    });
+
     app.post("/bikes", async (req, res) => {
       const bikeData = req.body;
       const bikes = await bikesCollection.insertOne(bikeData);
       res.send(bikes);
+    });
+
+    app.delete("/bikes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bikesCollection.deleteOne(query);
+      res.send(result);
     });
 
     app.get("/category", async (req, res) => {
@@ -89,9 +105,60 @@ async function run() {
       res.send(user);
     });
 
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = parseFloat(price) * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "USD",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const bikeId = payment.bikeId;
+      const query = { _id: ObjectId(id) };
+      const filter = { _id: ObjectId(bikeId) };
+      const updateDoc = {
+        $set: {
+          transactionId: payment.transactionId,
+          paid: true,
+        },
+      };
+      const updateSold = {
+        $set: {
+          sold: true,
+        },
+      };
+      const update = await bookingsCollection.updateOne(query, updateDoc);
+      const updateBike = await bikesCollection.updateOne(filter, updateSold);
       res.send(result);
     });
   } finally {
